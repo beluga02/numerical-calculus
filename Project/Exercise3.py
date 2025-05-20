4cd1b4fefe08c65b7555a5c9fbaead85aeec16b5
Letícia Matias <66281122+beluga02@users.noreply.github.com>
2025-05-20 06:46:51 +0300

    Update Exercise3.py

--- Project/Exercise3.py
+++ Project/Exercise3.py
@@ -1,5 +1,20 @@
 # Implementing the Gaussian elimination
 
+def matrix(n:int, q:float, p:float):
+  b = 1
+  a = 0
+  h=(b - a) / n
+  matriz = np.zeros(shape=(n - 1, n - 1))
+  for i in range(n - 1):
+      matriz[i, i] = 2 + (h ** 2) * q
+  for k in range(n - 2):
+      matriz[k + 1, k] = -1 - (h / 2) * p
+      matriz[k, k + 1] = -1 + (h / 2) * p
+        
+
+  return matriz
+
+
 def escalonamento(A):
   for j in range(0,len(A)):
     l = j
